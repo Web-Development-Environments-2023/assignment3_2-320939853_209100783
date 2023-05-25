e0c56bf7d9c8f7a425223a24bfcd5be29c534b88
TseytlinMark <mtseytlin345@gmail.com>
2023-05-25 19:33:32 +0300

    SSSs

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.post("/Register", async (req, res, next) => {
     let users = [];
     console.log(user_details)
     users = await DButils.execQuery("SELECT username from users");
-
+    console.log(users)
     if (users.find((x) => x.username === user_details.username))
       throw { status: 409, message: "Username taken" };
 
