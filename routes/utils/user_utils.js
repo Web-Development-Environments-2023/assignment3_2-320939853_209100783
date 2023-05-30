b23905a96cd728f887651cdced2736707aa3007a
TseytlinMark <mtseytlin345@gmail.com>
2023-05-30 22:27:41 +0300

    Before Merging With Eitan

--- routes/utils/user_utils.js
+++ routes/utils/user_utils.js
@@ -3,30 +3,12 @@ const fs = require('fs');
 async function markAsFavorite(user_id, recipe_id){
     await DButils.execQuery(`insert into FavoriteRecipes values ('${user_id}',${recipe_id})`);
 }
-
-async function getFavoriteRecipes(user_id){
-    const recipes_id = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
-    return recipes_id;
-}
-async function handleAddPrivateRecipeToUser(user_id, recipe){
-    let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe;
-    /**
-     * @TODO handle the sql query insert into the right tables
-     * @this await DButils.execQuery(`insert into FavoriteRecipes values ('${user_id}',${recipe_id})`);
-     */
-}
-async function handleFavoriteRecipesOfUser(userId){
-    const recipesList = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${userId}'`);
-    return recipesList;
-}
-
 async function handleDeleteFavoriteRecipesOfUser(user_id,recipeId){
     /**
      * @TODO create a sql query that delete this from the DB
      * const recipesList = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
      * return recipesList;
-     */
-    
+     */ 
 }
 async function handleDeleteFamilyRecipeOfUser(user_id,recipeId){
     /**
@@ -34,14 +16,104 @@ async function handleDeleteFamilyRecipeOfUser(user_id,recipeId){
      * const recipesList = await DButils.execQuery(`select recipe_id from FavoriteRecipes where user_id='${user_id}'`);
      * return recipesList;
      */
-    
 }
-async function handleAddRecipeToPersonal(currentUserId,recipeId)
+async function handleDeleteImage(imageName) {
+    fs.unlink(process.env.PATHIMAGES+imageName,function(err) {
+        if (err) {
+          throw err
+        } else {
+          return "Image Deleted successfully"
+        }
+    });
+
+}
+
+/**
+ * @description this section is for the Get requests of user 
+ * @method Get 
+ */
+
+
+
+async function handleGetFamilyRecipes(currentUserId)
 {
-    let queryExec = await DButils.execQuery(`INSERT INTO personal_recipes (user_id, recipe_id)
-    VALUES ('${currentUserId}', '${recipeId}')`);
-    return queryExec.data;
+    const recipeId_List = await DButils.execQuery(`select recipe_id from family_recipes where user_id='${currentUserId}'`);
+    console.log(recipeId_List);
+    if (recipeId_List.length > 0)
+    {return await handleGetRecipesOfDB(recipeId_List);}
+    else
+    {return null;}
 }
+async function handleGetPersonalRecipesOfUser(currentUserId)
+{
+    const recipeId_List = await DButils.execQuery(`select recipe_id from personal_recipes where user_id='${currentUserId}'`);
+    console.log(recipeId_List);
+    if (recipeId_List.length > 0)
+    {return await handleGetRecipesOfDB(recipeId_List);}
+    else
+    {return null;}
+}
+//TODO:
+async function handleGetFavoriteRecipesOfUser(userId){
+    const recipesList = await DButils.execQuery(`select recipe_id from favorite_recipes where user_id='${userId}'`);
+    //MORE COMPLEX, some recipes Might belong to DB and some might belong to API
+    return recipesList;
+}
+
+//TODO:
+async function handleGetLikedRecipesOfUser(userId){
+    const recipesList = await DButils.execQuery(`select recipe_id from liked_recipes where user_id='${userId}'`);
+    //MORE COMPLEX, some recipes Might belong to DB and some might belong to API
+    return recipesList;
+}
+//TODO:
+async function handleGetLastVisitedRecipes(userId, limit = 3) {
+    let query = `SELECT recipe_id FROM visited_recipes
+                 WHERE username = '${userId}'
+                 ORDER BY timestamp DESC
+                 LIMIT ${limit}`;
+    let result = await DButils.execQuery(query);
+    //MORE COMPLEX, some recipes Might belong to DB and some might belong to API
+    return result;
+}
+ 
+
+//TODO: EITAN 
+async function handleGetRecipesFromAPI(recipeId_ListFromAPI)
+{
+}
+
+
+/**
+ * @returns {recipes}, Array of Recipes JSONS, from DB !
+ */
+async function handleGetRecipesOfDB(recipeId_List)
+{
+    recipes = [];
+    for(let i = 0; i < recipeId_List.length; i++)
+    {
+        recipeId = recipeId_List[i].recipe_id;
+        let RecipeRow = await DButils.execQuery(`select * from recipes where recipe_id='${recipeId}'`);
+        //Query Returns From DB as Array
+        //We Select Only One Recipe At Time, So It is necessary to reach index [0]
+        RecipeRow = RecipeRow[0];
+        recipe = {recipe_id:RecipeRow.recipe_id, name:RecipeRow.name,Time:RecipeRow.Time,Likes:RecipeRow.Likes, isVegan:RecipeRow.isVegan, isVeget:RecipeRow.isVeget, isGfree:RecipeRow.isGfree, portions:RecipeRow.portions, Image:RecipeRow.Image, intolerances:RecipeRow.intolerances, Cuisine:RecipeRow.cuisine}
+        console.log(recipe);
+        let ingredient = await DButils.execQuery(`select * from ingredients where recipe_id='${recipeId}'`);
+        let steps = await DButils.execQuery(`select * from steps_recipes where recipe_id='${recipeId}'`);
+        let recipeJson = {recipe:recipe,ingredients:ingredient,steps:steps}
+        recipes.push(recipeJson);
+    }
+    return recipes;
+}
+
+
+/**
+ * @description this section is for the POST requests of user 
+ * @method Post 
+ */
+
+
 async function handleCreateRecipe(name,Time,Likes,isVegan,isVeget,isGfree,portions,image,intolerances,cuisine) {
 
     let recipe_info = await DButils.execQuery(
@@ -59,43 +131,17 @@ async function handleVisitRecipe(userId, recipeId, source) {
     let result = await DButils.execQuery(query);
     return result;
 }
+async function handleAddRecipeToPersonal(currentUserId,recipeId)
+{
+    let queryExec = await DButils.execQuery(`INSERT INTO personal_recipes (user_id, recipe_id)
+    VALUES ('${currentUserId}', '${recipeId}')`);
+    return queryExec.data;
+}
 
-async function handleGetLastVisitedRecipes(userId, limit = 3) {
-    let query = `SELECT recipe_id FROM visited_recipes
-                 WHERE username = '${userId}'
-                 ORDER BY timestamp DESC
-                 LIMIT ${limit}`;
-    let result = await DButils.execQuery(query);
-    return result;
-}
-/**
- * 
- * 
- * @todo Eitna finish this 
- */
-async function handleGetprivaterecipes(userId) {
-    let query = `SELECT * FROM recipes
-                 WHERE user_id = '${userId}'
-                 ORDER BY timestamp DESC
-                 LIMIT ${limit}`;
-    let result = await DButils.execQuery(query);
-    return result;
-}
-async function handleDeleteImage(imageName) {
-    fs.unlink(process.env.PATHIMAGES+imageName,function(err) {
-        if (err) {
-          throw err
-        } else {
-          return "Image Deleted successfully"
-        }
-    });
-
-}
 
 
 let functions = {
     markAsFavorite,
-    getFavoriteRecipes,
     handleAddPrivateRecipeToUser,
     handleFavoriteRecipesOfUser,
     handleDeleteFavoriteRecipesOfUser,
@@ -105,8 +151,9 @@ let functions = {
     handleVisitRecipe,
     handleGetLastVisitedRecipes,
     handleDeleteImage,
+    handleGetPersonalRecipesOfUser,
+    handleGetFamilyRecipes
 
 };
 exports.functions = functions
 exports.markAsFavorite = markAsFavorite;
-exports.getFavoriteRecipes = getFavoriteRecipes;
