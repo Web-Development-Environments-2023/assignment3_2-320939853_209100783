0e8be3a4d65582cb5f47d4e3060298d9441cdb65
TseytlinMark <mtseytlin345@gmail.com>
2023-06-17 11:43:33 +0300

    Endpoints maintaned

--- routes/recipes.js
+++ routes/recipes.js
@@ -9,17 +9,6 @@ router.get("/", (req, res) => res.send("im here"));
 const { currentUserId } = require('./auth.js');
 
 
-/**
- * This path returns a full details of a recipe by its id
- */
-// // router.get("/:recipeId", async (req, res, next) => {
-//   try {
-//     const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
-//     res.send(recipe);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 /**
  * @description this section is for the GET requests of user 
@@ -44,12 +33,23 @@ router.get("/randomrecipes", async (req, res, next) => {
 router.get("/getrecipe/:recipeId", async (req, res, next) => {
   try {
     let recipeId = req.params.recipeId;
-    let recipe = await recipes_utils.getRecipeDetails(recipeId);
-    res.send(recipe);
+    let source = req.query.src;
+    if(source == "API")
+    {
+      let recipe = await recipes_utils.getRecipeDetails(recipeId);
+      res.send(recipe);
+    }
+    else if(source =="Server"){
+      let recipe = await recipes_utils.getRecipeDetailsDB(recipeId);
+      res.send(recipe);
+    }
   } catch (error) {
     next(error);
   }
 });
+
+
+
 /**
  * @todo Write this endpoint in the yaml file 
  */
